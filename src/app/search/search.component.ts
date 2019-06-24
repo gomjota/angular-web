d1375c5c3cab31a6d5ba011c83f4007dc03fa290
juan1393 <j.gtrapero@gmail.com>
2019-06-24 08:49:27 +0200

    Search box added

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,23 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
